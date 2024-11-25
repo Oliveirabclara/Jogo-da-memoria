f41d13791fe42c7dc99abe14933a72f32ea565f8
Clara B. Oliveira <batistaoliveira.clara@gmail.com>
2024-11-24 19:08:39 -0500

    Criação dos cards do jogo com JS

---
+++ src/scripts/engine.js
@@ -0,0 +1,25 @@
+const emojis = [
+    "❤️",
+    "❤️",
+    "🙄",
+    "🙄",
+    "🧸",
+    "🧸",
+    "😄",
+    "😄",
+    "🌷",
+    "🌷",
+    "🪬",
+    "🪬",
+];
+let  openCards = [];
+
+let shuffleEmojis = emojis.sort(()=> (Math.random()> 0.5 ? 2: -1));
+
+for (let i=0; i<emojis.length; i++){
+
+    let box = document.createElement("div");
+    box.className = "item";
+    box.innerHTML = shuffleEmojis[i];
+    document.querySelector(".game").appendChild(box);
+}
